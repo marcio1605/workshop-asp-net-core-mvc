66c0b21e8caa857cf5e4c93c27c686d569ee46ef
Marcio <marcio1605@gmail.com>
2019-10-30 11:22:04 -0300

    others entities

---
+++ SalesWebMvc/Models/Enuns/SalesStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Models.Enuns
+{
+    public enum SalesStatus : int
+    {
+        Pending = 0,
+        Billed= 1,
+        Canceled = 2
+    }
+}
