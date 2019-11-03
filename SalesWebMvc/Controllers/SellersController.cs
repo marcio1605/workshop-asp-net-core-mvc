a3fdb9845d431a8c6dbeefaccf2d0ccd8c9f237c
Marcio <marcio1605@gmail.com>
2019-11-02 23:40:24 -0300

    Preparing sales search navigation views

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -29,7 +29,6 @@ namespace SalesWebMvc.Controllers
             return View(list);            
         }
 
-
         public async Task<IActionResult> Create()
         {
             var departaments = await _departamentService.FindAllAsync();
@@ -67,8 +66,16 @@ namespace SalesWebMvc.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Delete(int id)
         {
-            await _sellerService.RemoveAsync(id);
-            return RedirectToAction("Index");
+            try
+            {
+                await _sellerService.RemoveAsync(id);
+                return RedirectToAction("Index");
+            }
+            catch (IntegrityException e)
+            {
+                return RedirectToAction(nameof(Error), new { message = e.Message }); 
+            }
+
         }
 
         public async Task<IActionResult> Details(int? id)
